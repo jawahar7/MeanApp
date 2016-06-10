66915dd60d12054380025f7df0a9726c680f20be
Jawahar S <jawahar@GIHOS.COM>
2016-06-10 18:28:00 +0530

    change route

--- MongoMean/server/controller/user-controller.js
+++ MongoMean/server/controller/user-controller.js
@@ -3,6 +3,15 @@ var users = require('../model/users.js')
 var usercontroller = {};
 
 usercontroller.findall = function(req, res) {
+	users.find(function(err, data){
+		if(err)
+			res.send(err);
+		else
+			res.send(data);
+	});
+};
+
+usercontroller.save = function(req, res) {
 	var user = new users({username: 'jawahar', password: 'jawahar123', email: 'jawahar7.ceg@gmail.com'});
 	user.save(function(err, data){
 		if(err)
